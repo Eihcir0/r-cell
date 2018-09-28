fba3ab00fd08f4bc02d5586f59864061f9502e46
Eihcir0 <eihcir0@gmail.com>
2018-09-28 16:17:49 -0700

    Cleanup boilerplate

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Table from './components/Table'
 
 const App = () =>
   (<div style={{ width: 'max-content' }}>
+    <h1>Richie's Super Awesome Spreadsheet</h1>
     <Table x={26} y={26} />
   </div>)
 
